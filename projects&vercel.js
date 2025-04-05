f69d263cf8f887c46479312ec6e68091f94dcdd1
DevManTowhid <mdtowhidulalam364@gmail.com>
2025-04-05 14:12:09 +0000

    Add project showcase section with dynamic project listing

---
+++ projects&vercel.js
@@ -0,0 +1,12 @@
+const projects = [
+
+{
+    name: "my-profile",
+    github_link: "https://github.com/DevManTowhid/my_profile",
+    live_link: "https://my-profile-six-fawn.vercel.app/",
+    description: "A personal portfolio website to showcase my skills and projects. Built with HTML, Tailwind CSS, JS , and Vercel for deployment."
+}
+
+
+]
+
